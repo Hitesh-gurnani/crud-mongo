f204f2e5dbc8b6b95a90ad3a75b1281398b35486
Hitesh Gurnani <55234408+Hitesh-gurnani@users.noreply.github.com>
2022-01-04 18:32:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,11 @@ const { log } = require('console');
 const express = require('express');
 const app = express();
 app.use(express.json());
-//let users = [{ id: '1', 'name': "Abhiskhek" }, { id: '2', 'name': "Hitesh" }, { id: '3', 'name': "aazz" }];
+
 const userRouter = express.Router();
 const authRouter = express.Router();
 const mongoose = require('mongoose')
-app.use('/auth', authRouter)
 app.use('/user', userRouter)
-// authRouter
-//     .route('/signup')
-//     .get(middleware1, getsigup, middleware2)
-//     .post(postSignup)
 
 userRouter
     .route('/')
@@ -20,45 +15,6 @@ userRouter
     .patch(updateUser)
     .delete(deleteuser)
 
-// userRouter
-//     .route("/:id")
-//     .get(getId)
-// function getsigup(req, res, next) {
-//     console.log('get sign up called');
-//     //res.sendFile('index.html', { root: __dirname })
-//     next();
-// }
-// function postSignup(req, res) {
-//     let obj = req.body;
-//     console.log('====================================');
-//     console.log(obj);
-//     console.log('====================================');
-//     res.json({
-//         "mssg": "post request success",
-//         "data": obj
-//     })
-// }
-// // app.get('/', function (req, res) {
-// //     res.sendFile('C:/Users/ASUS/OneDrive/Desktop/BKEND/views/index.html')
-// // });
-// // app.get('/about', function (req, res) {
-// //     res.sendFile('./views/about.html', { root: __dirname })
-// // })
-// // app.get('/about-us', (req , res) => {
-// //     res.redirect('/about');
-// // })
-// // app.use((req, res) => {
-// //     res.status(404).sendFile('./views/404.html', { root: __dirname })
-// // })
-// function middleware1(req, res, next) {
-//     console.log('middleware1 called');
-//     next();
-// }
-// function middleware2(req, res, next) {
-//     console.log('middleware2 called');
-//     res.sendFile('index.html', { root: __dirname })
-
-// }
 async function getUser(req, res) {
     let allusers = await userModel.findOne({ name: 'jasbeeer' })
     res.json({
@@ -86,9 +42,6 @@ async function updateUser(req, res) {
         message: "data updated successfully :)"
     })
 }
-
-
-// }
 async function deleteuser(req, res) {
     let datatobedeleted = req.body;
     const data = await userModel.findOneAndDelete(datatobedeleted)
@@ -97,22 +50,8 @@ async function deleteuser(req, res) {
         "data": data
     })
 }
-// function getId(req, res) {
-//     console.log(req.params.id);
-//     let paramsId = req.params.id;
-//     let obj = {}
-//     for (let i = 0; i < users.length; i++) {
-//         if (users[i]['id'] == paramsId)
-//             obj = users[i];
-//     }
-//     res.json({
-//         "mssg": "Id fetched successfully",
-//         "data": obj
-//     })
-// }
+
 const db_link = 'mongodb://127.0.0.1:27017'
-
-
 mongoose.connect(db_link, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(function () {
         console.log('db connect')
@@ -147,14 +86,5 @@ const userSchema = mongoose.Schema({
 })
 
 const userModel = mongoose.model('userModel', userSchema);
-// (async function createUser() {
-//     let user = {
-//         name: 'jasbeeer',
-//         email: 'nw@gmail.com',
-//         password: 'qweewe',
-//         confirmPassword: 'qweewe'
-//     };
-//     let data = await userModel.create(user);
-//     console.log(data);
-// })();
+
 app.listen(3000);
